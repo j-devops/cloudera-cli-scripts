b1710b5ed24b47e34760d80fa52aad474023d2fe
root <root@ip-172-31-2-196.ec2.internal>
2015-07-23 20:31:46 +0000

    added ssh key copy

---
+++ ssh_key_copy.sh
@@ -0,0 +1,5 @@
+cat ~/.ssh/id_rsa.pub | ssh -i /root/DEV_KP.pem root@172.31.2.193 'umask 0077; mkdir -p .ssh; cat >> .ssh/authorized_keys && echo "Key copied"'
+cat ~/.ssh/id_rsa.pub | ssh -i /root/DEV_KP.pem root@172.31.2.194 'umask 0077; mkdir -p .ssh; cat >> .ssh/authorized_keys && echo "Key copied"'
+cat ~/.ssh/id_rsa.pub | ssh -i /root/DEV_KP.pem root@172.31.2.195 'umask 0077; mkdir -p .ssh; cat >> .ssh/authorized_keys && echo "Key copied"'
+cat ~/.ssh/id_rsa.pub | ssh -i /root/DEV_KP.pem root@172.31.2.196 'umask 0077; mkdir -p .ssh; cat >> .ssh/authorized_keys && echo "Key copied"'
+cat ~/.ssh/id_rsa.pub | ssh -i /root/DEV_KP.pem root@172.31.2.197 'umask 0077; mkdir -p .ssh; cat >> .ssh/authorized_keys && echo "Key copied"'
