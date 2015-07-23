3e5d17f5e7cb1bdc221d469fecb0babc86a92139
root <root@ip-172-31-2-193.ec2.internal>
2015-07-23 20:45:32 +0000

    a

--- ssh_key_copy.sh
+++ ssh_key_copy.sh
@@ -1,3 +1,4 @@
+ssh-keygen -t rsa
 cat ~/.ssh/id_rsa.pub | ssh -i /root/DEV_KP.pem root@172.31.2.193 'umask 0077; mkdir -p .ssh; cat >> .ssh/authorized_keys && echo "Key copied"'
 cat ~/.ssh/id_rsa.pub | ssh -i /root/DEV_KP.pem root@172.31.2.194 'umask 0077; mkdir -p .ssh; cat >> .ssh/authorized_keys && echo "Key copied"'
 cat ~/.ssh/id_rsa.pub | ssh -i /root/DEV_KP.pem root@172.31.2.195 'umask 0077; mkdir -p .ssh; cat >> .ssh/authorized_keys && echo "Key copied"'
